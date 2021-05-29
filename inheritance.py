87b70cdb656ecf34a8f0fd923e92d3ec7b7e9208
unknown <pawarmayuri681@gmail.com>
2021-05-29 16:57:03 +0530

    first commit

---
+++ inheritance.py
@@ -0,0 +1,66 @@
+
+
+class Animal:  
+    def speak(self):  
+        print("Animal Speaking")  
+#child class Dog inherits the base class Animal  
+class Dog(Animal):  
+    def bark(self):  
+        print("dog barking")  
+d = Dog()  
+d.bark()  
+d.speak()  
+
+
+
+#Multilevel Inheritance
+
+class Animal:  
+    def speak(self):  
+        print("Animal Speaking")  
+#The child class Dog inherits the base class Animal  
+class Dog(Animal):  
+    def bark(self):  
+        print("dog barking")  
+#The child class Dogchild inherits another child class Dog  
+class DogChild(Dog):  
+    def eat(self):  
+        print("Eating bread...")  
+d = DogChild()  
+d.bark()  
+d.speak()  
+d.eat()  
+
+
+#Multiple Inheritance
+
+class Calculation1:  
+    def Summation(self,a,b):  
+        return a+b;  
+class Calculation2:  
+    def Multiplication(self,a,b):  
+        return a*b;  
+class Derived(Calculation1,Calculation2):  
+    def Divide(self,a,b):  
+        return a/b;  
+d = Derived()  
+print(d.Summation(10,20))  
+print(d.Multiplication(10,20))  
+print(d.Divide(10,20)) 
+
+
+#Method Overiding
+class Bank:  
+    def getroi(self):  
+        return 10;  
+class SBI(Bank):  
+    def getroi(self):  
+        return 7;  
+  
+class ICICI(Bank):  
+    def getroi(self):  
+        return 8;  
+b1 = Bank()  
+b2 = SBI()  
+b3 = ICICI()  
+
